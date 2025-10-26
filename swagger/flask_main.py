dd25c461be28783bd85743104a2826e424ce4b58
Minh Pham <phamvanminh272@gmail.com>
2025-10-26 10:11:30 +0700

    Fix swagger doesn't show responses

--- swagger/flask_main.py
+++ swagger/flask_main.py
@@ -9,30 +9,39 @@ swagger_bp = Blueprint("swagger_bp", __name__)
 @swagger_bp.route("/")
 def swagger_ui():
     html = """
-        <!-- templates/swagger_ui.html -->
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Swagger UI</title>
-      <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css">
-    </head>
-    <body>
-      <div id="swagger-ui"></div>
-      <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
-      <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-standalone-preset.js"></script>
-      <script>
-        window.onload = function() {
-          SwaggerUIBundle({
-            url: "{{ url_for('swagger_bp.get_spec') }}",
-            dom_id: '#swagger-ui',
-            presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
-            layout: "BaseLayout"
-          });
-        };
-      </script>
-    </body>
-    </html>
-    """
+            <!-- templates/swagger_ui.html -->
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Swagger UI</title>
+          <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.19.5/swagger-ui.css" >
+          <style>
+            .topbar {
+              display: none;
+            }
+          </style>
+        </head>
+        <body>
+          <div id="swagger-ui"></div>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.19.5/swagger-ui-bundle.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.19.5/swagger-ui-standalone-preset.js"></script>
+          <script>
+            window.onload = function() {
+              const ui = SwaggerUIBundle({
+                url: "{{ url_for('swagger_bp.get_spec') }}",
+                dom_id: '#swagger-ui',
+                presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+                plugins: [
+                SwaggerUIBundle.plugins.DownloadUrl
+                ],
+                layout: "StandaloneLayout"
+              });
+              window.ui = ui
+            };
+          </script>
+        </body>
+        </html>
+        """
     return render_template_string(html)
 
 
